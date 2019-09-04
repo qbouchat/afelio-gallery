1ef0e616630325b0be8c46cd2348b07637f6919b
Axel Herman <a.herman@afelio.be>
2019-09-04 11:11:47 +0200

    make options optional...

--- src/utils/interface/afelio-gallery-options.interface.ts
+++ src/utils/interface/afelio-gallery-options.interface.ts
@@ -7,22 +7,22 @@ export interface AfelioGalleryOptions {
 	//  ICONS SOURCES PROPS
 	//
 	///////
-	previousIconUrl: string;
-	nextIconUrl: string;
-	rotateIconUrl: string;
-	closeIconUrl: string;
-	deleteIconUrl: string;
+	previousIconUrl?: string;
+	nextIconUrl?: string;
+	rotateIconUrl?: string;
+	closeIconUrl?: string;
+	deleteIconUrl?: string;
 
 	///////
 	//
 	//  BOOLEAN PROP
 	//
 	///////
-	enableDelete: boolean;
-	enableVisualDelete: boolean;
-	enableRotate: boolean;
-	enableBackdropClickClose: boolean;
+	enableDelete?: boolean;
+	enableVisualDelete?: boolean;
+	enableRotate?: boolean;
+	enableBackdropClickClose?: boolean;
 
 	// CUSTOM ACTIONS
-	actions: Action[];
+	actions?: Action[];
 }
