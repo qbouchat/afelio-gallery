80a119aec774eef0b2c5172322e16ae477d82f58
Axel Herman <a.herman@afelio.be>
2019-09-04 11:15:14 +0200

    build

--- dist/types/utils/interface/afelio-gallery-options.interface.d.ts
+++ dist/types/utils/interface/afelio-gallery-options.interface.d.ts
@@ -1,13 +1,13 @@
 import { Action } from "../../models/action.model";
 export interface AfelioGalleryOptions {
-    previousIconUrl: string;
-    nextIconUrl: string;
-    rotateIconUrl: string;
-    closeIconUrl: string;
-    deleteIconUrl: string;
-    enableDelete: boolean;
-    enableVisualDelete: boolean;
-    enableRotate: boolean;
-    enableBackdropClickClose: boolean;
-    actions: Action[];
+    previousIconUrl?: string;
+    nextIconUrl?: string;
+    rotateIconUrl?: string;
+    closeIconUrl?: string;
+    deleteIconUrl?: string;
+    enableDelete?: boolean;
+    enableVisualDelete?: boolean;
+    enableRotate?: boolean;
+    enableBackdropClickClose?: boolean;
+    actions?: Action[];
 }
